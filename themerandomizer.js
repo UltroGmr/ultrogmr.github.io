7ccbfc3df35de53676c30c0c4959a764fa9fa3ac
ultrogmr@gmail.com <unconfigured@null.spigotmc.org>
2025-10-06 18:25:31 -0400

    New theme

--- themerandomizer.js
+++ themerandomizer.js
@@ -6,7 +6,7 @@ if (urlParams.has("theme")) {
   let themeId = urlParams.get("theme");
   document.getElementById("styles").href = themeId;
 } else {
-  let themeId = themes[ Math.floor(Math.random() * 2.0) ];
+  let themeId = themes[ Math.floor(Math.random() * 3.0) ];
   urlParams.set("theme", themeId);
   document.getElementById("styles").href = themeId;
   console.log(themeId);
