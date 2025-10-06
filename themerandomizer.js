9c6076a726cde6c752c60900240f4997e9424435
UltroGmr <164104970+UltroGmr@users.noreply.github.com>
2025-10-06 17:43:12 -0400

    Update themerandomizer.js

--- themerandomizer.js
+++ themerandomizer.js
@@ -1,1 +1,12 @@
+themes = ["water.css", "lava.css"]
 
+const urlParams = new URLSearchParams(window.location.search);
+
+if urlParams.has("theme") {
+  let themeId = urlParams.get("theme");
+  document.getElementById("styles").href = themeId
+} else {
+  let themeId = themes[ Math.floor(Math.random() * 2.0) ]
+  urlParams.set("theme", themeId)
+  document.getElementById("styles").href = themeId
+}
